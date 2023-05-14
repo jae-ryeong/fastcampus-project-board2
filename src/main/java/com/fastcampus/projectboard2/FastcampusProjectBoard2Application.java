99c4f1ed3ff9680bfe353fb176e07589c8375c47
김재령 <wofud0321@naver.com>
2023-05-14 23:09:57 +0900

    Initial commit

---
+++ src/main/java/com/fastcampus/projectboard2/FastcampusProjectBoard2Application.java
@@ -0,0 +1,13 @@
+package com.fastcampus.projectboard2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusProjectBoard2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusProjectBoard2Application.class, args);
+    }
+
+}
