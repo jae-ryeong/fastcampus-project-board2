1926b6d08efac1be734d058b3483ebd7c6bc7990
김재령 <wofud0321@naver.com>
2023-05-17 15:23:27 +0900

    spring Data Rest 테스트 제외처리

--- src/test/java/com/fastcampus/projectboard2/controller/DataRestTest.java
+++ src/test/java/com/fastcampus/projectboard2/controller/DataRestTest.java
@@ -1,5 +1,6 @@
 package com.fastcampus.projectboard2.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
+@Disabled("spring data rest 통합테스트는 불필요하므로 제외시킴")
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
