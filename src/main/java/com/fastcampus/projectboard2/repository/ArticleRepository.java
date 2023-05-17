41bac69714566904883b903d01b828365dbebffc
김재령 <wofud0321@naver.com>
2023-05-17 16:02:50 +0900

    querydsl을 이용해 api 검색 옵션 추가

--- src/main/java/com/fastcampus/projectboard2/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/projectboard2/repository/ArticleRepository.java
@@ -1,12 +1,28 @@
 package com.fastcampus.projectboard2.repository;
 
 import com.fastcampus.projectboard2.domain.Article;
+import com.fastcampus.projectboard2.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long>
+public interface ArticleRepository extends JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,
+       QuerydslBinderCustomizer<QArticle>
 {
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        bindings.excludeUnlistedProperties(true);   // 필터를 이용해 검색하고 싶을때
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy); // 어떤필드를 필터로 사용할지 추가
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);  // title, 대소문자 구별 X
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq); // API구현 29분
+    }
 }
