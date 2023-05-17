7b04a9e7c74200a475ae0372837132833a3b136a
김재령 <wofud0321@naver.com>
2023-05-17 19:03:48 +0900

    컨트롤러 테스트 업데이트

--- src/test/java/com/fastcampus/projectboard2/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcampus/projectboard2/controller/ArticleControllerTest.java
@@ -41,7 +41,7 @@ class ArticleControllerTest {
         //when & then
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
@@ -54,7 +54,7 @@ class ArticleControllerTest {
         //when & then
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))  // view이므로 text_html
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))  // view이므로 text_html
                 .andExpect(model().attributeExists("articles/search"));
     }
 
@@ -65,7 +65,7 @@ class ArticleControllerTest {
         //when & then
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("articles/search-hashtag"));
     }
 }
