40784a1fdcbe49b890cc2b725e4aa496530461d6
김재령 <wofud0321@naver.com>
2023-05-17 23:20:27 +0900

    시큐리티 설정에 모든 요청에 인증이 열리도록 설정

--- src/main/java/com/fastcampus/projectboard2/config/SecurityConfig.java
+++ src/main/java/com/fastcampus/projectboard2/config/SecurityConfig.java
@@ -1,5 +1,18 @@
 package com.fastcampus.projectboard2.config;
 
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {    //무조건 /login 페이지가 뜨지 않게 해준다.
+        return http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .formLogin().and()
+                .build();
+    }
 }
