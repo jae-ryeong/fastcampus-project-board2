8d33e51938baec945fa6992602426d8380cf0676
김재령 <wofud0321@naver.com>
2023-05-17 23:21:13 +0900

    로그인 컨트롤러 테스트 반영

--- src/main/java/com/fastcampus/projectboard2/config/SecurityConfig.java
+++ src/main/java/com/fastcampus/projectboard2/config/SecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-
+    //시큐리티 설정에 모든 요청에 인증이 열리도록 설정
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {    //무조건 /login 페이지가 뜨지 않게 해준다.
         return http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
