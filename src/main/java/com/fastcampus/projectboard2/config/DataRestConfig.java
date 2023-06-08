28ef748717240b277d50f9a9d54fc06cacbb5edd
김재령 <wofud0321@naver.com>
2023-06-08 23:28:59 +0900

    주석추가

--- src/main/java/com/fastcampus/projectboard2/config/DataRestConfig.java
+++ src/main/java/com/fastcampus/projectboard2/config/DataRestConfig.java
@@ -11,6 +11,7 @@ public class DataRestConfig {
     @Bean
     public RepositoryRestConfigurer repositoryRestConfigurer() {
         return RepositoryRestConfigurer.withConfig((config, cors) ->
-                config.exposeIdsFor(UserAccount.class));
+                config.exposeIdsFor(UserAccount.class));    // UserAccount 클래스의 ID를 노출(expose)하도록 구성한다.
+            //  Spring Data REST가 UserAccount 엔티티의 ID 필드를 클라이언트에게 노출하게 된다.
     }
 }
