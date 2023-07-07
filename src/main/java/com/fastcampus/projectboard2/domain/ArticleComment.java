640f38915b668cd2075bf54f7fb0f82272b88ff5
김재령 <wofud0321@naver.com>
2023-07-07 23:18:28 +0900

    #85 - 엔티티 equals(), hashcode() 비교 로직에 getter 적용
    여기서 필드에 직접 접근하면,
    하이버네이트가 지연 로딩하려고 만든 프록시 객체를 다룰 때
    필드 값이 `null`일 수 있음
    그러면 제대로 비교 로직을 수행할 수 없기 떄문에
    getter를 이용함.
    이것으로 프록시 객체를 타더라도 제대로 값을 읽어올 수 있음

--- src/main/java/com/fastcampus/projectboard2/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/projectboard2/domain/ArticleComment.java
@@ -48,12 +48,12 @@ public class ArticleComment extends AuditingFields {   // 댓글
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ArticleComment that = (ArticleComment) o;
-        return id != null && id.equals(that.getId());
+        return this.getId() != null && this.getId().equals(that.getId());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return Objects.hash(this.getId());
     }
 }
 
