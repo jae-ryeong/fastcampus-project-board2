23a6ba0d321783a9353e97e070907d2e3ea3f317
김재령 <wofud0321@naver.com>
2023-07-07 21:44:08 +0900

    #62 - AuthControllerTest: Void.class 표현 개선
    원래 이 자리에는 아무 클래스가 아니라 빈이 들어가야 한다.
    `Void`는 스프링 빈이 아니므로 정확한 표현이 아니고
    일부 개발 환경에서 에러를 유발하는 것을 확인
    이에 테스트 전용 빈을 제대로 만들어서 넣는 방법으로 개선.
    테스트 빈은 아무 일도 하지 않는 것이 목적이기 때문에
    안에 내용을 가지고 있지 않다.
    다른 테스트에서 재활용할 가능성이 적은 점을 고려해
    inner class 로 작성.

--- src/test/java/com/fastcampus/projectboard2/controller/AuthControllerTest.java
+++ src/test/java/com/fastcampus/projectboard2/controller/AuthControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestComponent;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -15,7 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @DisplayName("View 컨트롤러 - 인증")
 @Import(SecurityConfig.class)
-@WebMvcTest(Void.class)
+@WebMvcTest(AuthControllerTest.EmptyController.class)
 public class AuthControllerTest {
 
     private final MockMvc mvc;
@@ -33,4 +34,10 @@ public class AuthControllerTest {
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andDo(MockMvcResultHandlers.print());        // 테스트에서 HTTP 상태, 헤더 및 본문을 포함하여 요청 및 응답의 세부 정보를 인쇄하여 테스트를 실행할 때 출력을 검사
     }
+
+    /**
+     * 어떤 컨트롤러도 필요하지 않은 테스트임을 나타내기 위해 테스트용 빈 컴포넌트를 사용함.
+     */
+    @TestComponent
+    static class EmptyController {}
 }
