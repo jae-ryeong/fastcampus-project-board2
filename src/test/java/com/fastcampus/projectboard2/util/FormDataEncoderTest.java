febcddc1451793c168605563259cbefe268519f3
김재령 <wofud0321@naver.com>
2023-07-07 21:49:26 +0900

    #62 - FormDataEncoderTest: Void.class 표현 개선
    이 테스트는 사용하지 않는 빈이 아예 없는게 아니라
    테스트 대상인 `FormDataEncoder`,
    잭슨 `ObjectMapper`를 사용함
    `@SpringBootTest`를 사용한다면,
    이들을 `@Import`로 넣는 것보다 이 변경점과 같이
    더 알맞은 방법을 사용할 수 있음

--- src/test/java/com/fastcampus/projectboard2/util/FormDataEncoderTest.java
+++ src/test/java/com/fastcampus/projectboard2/util/FormDataEncoderTest.java
@@ -13,8 +13,10 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("테스트 도구 - Form 데이터 인코더")
-@Import({FormDataEncoder.class, ObjectMapper.class})
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.NONE,
+        classes = {FormDataEncoder.class, ObjectMapper.class}
+)
 public class FormDataEncoderTest {  // 테스트가 실패한다.
 
     private final FormDataEncoder formDataEncoder;
